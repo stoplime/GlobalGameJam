341fb653d631f45717b687010da15dac40e8543d
stoplime <slanimero@gmail.com>
2018-01-27 12:34:23 -0500

    wave new material

--- Scripts/Wave.cs
+++ Scripts/Wave.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wave : MonoBehaviour {
+	public Shader shader;
+	public Color color;
+	public float frequency;
+	public float thickness;
+	public float offset;
 
 	// Use this for initialization
 	void Start () {
-		
+		Renderer rend = GetComponent<Renderer>();
+        rend.material = new Material(shader);
+		rend.material.color = color;
+		rend.material.SetFloat("_Frequency", frequency);
+		rend.material.SetFloat("_Thickness", thickness);
+		rend.material.SetFloat("_Offset", offset);
 	}
 	
 	// Update is called once per frame
