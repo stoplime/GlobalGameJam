6355195286486ecda1e0b4b33f822c20749eacbd
Nhorsma <35874942+Nhorsma@users.noreply.github.com>
2018-01-27 14:55:41 -0500

    psuedocode for dot entity
    rought psuedo code for dot entity stuff

--- Scripts/DotEntity.cs
+++ Scripts/DotEntity.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 
 public class DotEntity : MonoBehaviour {
 
-	// Use this for initialization
+	private float repel;
+	private Vector3 color;
+	private string emot;
+	private float freq;
+	private float ampl;
+	private float noise;
+
+	void getRepelStrength(){
+
+	}
+
+	void getColor(){
+		//angry.color(255,25,50) and (175, 0, 75)
+		//sad.color(50, 50, 175) and (25, 25, 125)
+		//fear.color(175, 255, 100) and (100, 175, 100)
+	}
+
+	void transition{
+		//rate of transition = (3-distance/2)
+		//calculates distance from another dot to determine rate of change to color of close dot
+		//	if(distance<tooFar from otherDot){ return dot.rgb , return otherDot.rgb
+		//	dot.transition*(distance-tooFar) , otherDot.transision*(distance-tooFar)}
+	}
+
+
+
+
 	void Start () {
 		
 	}
-	
+
+
+
 	// Update is called once per frame
 	void Update () {
 		
