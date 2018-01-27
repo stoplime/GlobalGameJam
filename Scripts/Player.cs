9da0d92e967637573f09391f1f17cd94cb660f26
Christian Byrne <christianbyrne2012@gmail.com>
2018-01-27 14:33:30 -0500

    implement inertia

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public float speed;
-	private Vector2 vel;
+    public const float FRICTION_CONSTANT = 2F;
+    public const float FORCE = 0.25F;
+
+    public float speed;     // the speed scalar
+    private Vector2 vel;    // velocity vector
+    private Vector2 acl;    // acceleration vector
+    private Vector2 net;    // net force vector
+    private Vector2 frc;    // actor force vector
+    private Vector2 fri;    // friction vector
 
 	// Use this for initialization
 	void Start () {
 		vel = Vector2.zero;
+        acl = Vector2.zero;
+        net = Vector2.zero;
+        frc = Vector2.zero;
+        fri = Vector2.zero;
 	}
 	
 	// Update is called once per frame
@@ -16,35 +27,62 @@ public class Player : MonoBehaviour {
 		if (!GameManage.IsPaused)
 		{
 			Controls();
-			transform.Translate(vel);
+            calculateFriction();
+            calculateNetForce();
+            calculateVelocity();
+            transform.Translate(vel);
 		}
 	}
+
+    private void calculateVelocity()
+    {
+        vel.x += Time.deltaTime * net.x;
+        vel.y += Time.deltaTime * net.y;
+    }
+
+    private void calculateFriction()
+    {
+        fri.x = FRICTION_CONSTANT * (vel.x/speed);
+        fri.y = FRICTION_CONSTANT * (vel.y/speed);
+    }
+
+    private void calculateNetForce()
+    {
+        net.x = frc.x - fri.x;
+        net.y = frc.y - fri.y;
+    }
 
 	public void Controls() {
 		if (Input.GetKey(GameManage.keyProfile["right"]))
 		{
-			vel.x = Time.deltaTime * speed;
+            frc.x = FORCE;
+			// vel.x = Time.deltaTime * speed;
 		}
 		else if (Input.GetKey(GameManage.keyProfile["left"]))
 		{
-			vel.x = -Time.deltaTime * speed;
+            frc.x = -FORCE;
+			// vel.x = -Time.deltaTime * speed;
 		}
 		else
 		{
-			vel.x = 0;
+            frc.x = 0;
+			// vel.x = 0;
 		}
 
 		if (Input.GetKey(GameManage.keyProfile["forward"]))
 		{
-			vel.y = Time.deltaTime * speed;
+            frc.y = FORCE;
+			//vel.y = Time.deltaTime * speed;
 		}
 		else if (Input.GetKey(GameManage.keyProfile["backward"]))
 		{
-			vel.y = -Time.deltaTime * speed;
+            frc.y = -FORCE;
+			//vel.y = -Time.deltaTime * speed;
 		}
 		else
 		{
-			vel.y = 0;
+            frc.y = 0;
+			// vel.y = 0;
 		}
 	}
 }
