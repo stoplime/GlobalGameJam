77d890222e01c95bb467ab287155076f7bdc5740
Christian Byrne <christianbyrne2012@gmail.com>
2018-01-28 12:34:04 -0500

    still broken at runtime, but it shouldn't affect behavior

--- Scripts/Item.cs
+++ Scripts/Item.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class Item : MonoBehaviour {
     public bool pickedUp = false;
 
+    private float rotationSpeed;
+
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
     void Start()
     {
-        
+        rotationSpeed = 60.0F;
+    }
+
+    void CheckDistance()
+    {
+        const float _A = 12.2F;
+        const float _B = 10F;
+        const float _C = 3.5F;
+        //print(GameManager.player);
+        float distance = Help.getDist2D(transform.position, GameManager.player.transform.position) * 2;
+
+        rotationSpeed = -_A * Help.getTanH(distance/_B - _C) + 61F + _A;
     }
 
     void Update () {
-        transform.Rotate(new Vector3(0, 0, 60)*Time.deltaTime);
+        CheckDistance();
+        transform.Rotate(new Vector3(0F, 0F, rotationSpeed)*Time.deltaTime);
+        //CheckDistance();
     }
 
     
