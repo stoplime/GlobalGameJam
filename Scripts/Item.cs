9cb3987663cfa81cf1d3f2e33b08ac185eb08ce7
Nhorsma <35874942+Nhorsma@users.noreply.github.com>
2018-01-28 12:26:38 -0500

    newscenes

--- Scripts/Item.cs
+++ Scripts/Item.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour {
-    public bool pickedUp = false;
+public class Item : MonoBehaviour
+{
 
-    /// <summary>
-    /// Start is called on the frame when a script is enabled just before
-    /// any of the Update methods is called the first time.
-    /// </summary>
-    void Start()
+
+    void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 0, 60) * Time.deltaTime);
     }
 
-    void Update () {
-        transform.Rotate(new Vector3(0, 0, 60)*Time.deltaTime);
-    }
+   // private void Start()
+   // {
+       // Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        //Vector3 itemPos = GameObject.FindGameObjectWithTag("Item").transform.position;
 
-    
+       // float distance = Vector3.Distance(playerPos, itemPos);
+      //  if (distance < 1)
+      //  {
+           // rotateTransform.RotateAround(playerPos, Vector3.up, 2 * Time.deltaTime);
+          //  transform.RotateAround(itemPos, playerPos, 20 * Time.deltaTime);
 
+            //Vector3 relativePos = (playerPos + new Vector3(0, 1.5f, 0)) - transform.position;
+            // Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+            // Quaternion current = transform.localRotation;
+
+            //  transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+            //   transform.Translate(0, 0, 3 * Time.deltaTime);
+      //  } 
+   // }
 }
